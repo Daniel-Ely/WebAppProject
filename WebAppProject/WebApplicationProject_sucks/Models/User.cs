93c9ec11cc8d35317c4c80f7b974bd76760ad53d
lenoyOst <lenoy.ost@gmail.com>
2020-07-30 12:34:01 +0300

    adding loginc to the register page

--- WebAppProject/WebApplicationProject_sucks/Models/User.cs
+++ WebAppProject/WebApplicationProject_sucks/Models/User.cs
@@ -5,19 +5,23 @@ using System.ComponentModel.DataAnnotations;
 namespace WebApplicationProject_sucks.Models
 {
     public class User
-    {     
-       [Key] public string UserName { get; set; }
-
+    {
+        [Display(Name = "User name")]
+        [Key] public string UserName { get; set; }
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
-
+       
         public string Gender { get; set; }
 
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)]
         public DateTime BirthDay { get; set; }
 
-        public virtual ICollection<UserToCategory> Interests { get; set; }     
+        public virtual ICollection<UserToCategory> Interests { get; set; }
 
+        [Display(Name = "Email address")]
         public string Email { get; set; }
-
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public Image ProfileImage { get; set; }
