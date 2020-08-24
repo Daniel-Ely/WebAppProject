d104c5b0fa135441e29553057a7bfae740d41ebd
lenoyOst <lenoy.ost@gmail.com>
2020-08-24 10:23:28 +0300

    Adding filterAtributte in order to montitur the user catgorys of intrest

--- project/WebAppProject/WebApplicationProject_sucks/UserActivityFilterAttribute.cs
+++ project/WebAppProject/WebApplicationProject_sucks/UserActivityFilterAttribute.cs
@@ -4,31 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationProject_sucks.Controllers;
+using WebApplicationProject_sucks.Models;
 
 namespace WebApplicationProject_sucks
 {
     public class UserActivityFilterAttribute : ActionFilterAttribute
     {
+        private MyDB db = new MyDB();
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+
             //
-            //getting the Qroon/Post id
+            //getting the Qroom/Post id
             //
-            var test = filterContext.ActionParameters;
+            var id = filterContext.Controller.ValueProvider.GetValue("Id").AttemptedValue;
             //
-            //getting the controller 
+            //getting the controller in order to know if the user enterd a post or a Qroom
             //
             var controller = filterContext.Controller;
             if (controller.GetType() == typeof(QuestionRoomsController))
             {
                 controller = (QuestionRoomsController)controller;
-                
+                QuestionRoom questionRoom = db.QuestionRooms.Find(id);
+                foreach (RoomToCategory c in questionRoom.Categories)
+                {
+                    //
+                    //in case the catgory is alrea
+                    //
+                }
             }
-            
+
         }
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-           
+
         }
     }
 }
