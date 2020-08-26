9f2ff8e856d6df496a44c226458bc23095368761
lenoyOst <lenoy.ost@gmail.com>
2020-08-26 15:34:13 +0300

    ariel help me ;)

--- project/WebAppProject/WebApplicationProject_sucks/Controllers/AdminController.cs
+++ project/WebAppProject/WebApplicationProject_sucks/Controllers/AdminController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebApplicationProject_sucks.Models;
 
@@ -16,6 +15,7 @@ namespace WebApplicationProject_sucks.Controllers
   
         public ActionResult AdminStatistics()
         {
+            
             return View();
         }
     
