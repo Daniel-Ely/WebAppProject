a28102cd954631dd411f3c29777bbb424020b326
S-S-H <54595997+S-S-H@users.noreply.github.com>
2020-08-28 22:07:09 +0300

    stick graph fixed

--- project/WebAppProject/WebApplicationProject_sucks/Scripts/ActivityStickGraph.js
+++ project/WebAppProject/WebApplicationProject_sucks/Scripts/ActivityStickGraph.js
@@ -1,7 +1,7 @@
 ﻿//parse the data from view
 
-var professionList = document.getElementsByName('ProfessionName');
-var professionList = document.getElementsByName('ProfessionCount');
+var professionData = document.getElementsByName('ProfessionName');
+var countData = document.getElementsByName('ProfessionCount');
 
 
 var data = [];
@@ -9,9 +9,9 @@ var data = [];
 // set "dict" data
 for (var i = 0; i < professionData.length; i++) {
     data.push({
-        Profession: professionData.item(i).nodeValue,
-        Count: countData.item(i).nodeValue
-        //count: countData.item(i).attributes.getNamedItem("value").nodeValue,
+        Profession: professionData.item(i).attributes.getNamedItem("value").nodeValue,
+        Count: countData.item(i).attributes.getNamedItem("value").nodeValue
+        
     })
 }
 
@@ -30,8 +30,6 @@ var svg = d3.select("#my_BarViz")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
-
-
 
 
     // sort data
@@ -53,7 +51,7 @@ var svg = d3.select("#my_BarViz")
     
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([0, 500])
+        .domain([0, 50])
         .range([height, 0]);
     svg.append("g")
         .call(d3.axisLeft(y));
@@ -68,4 +66,3 @@ var svg = d3.select("#my_BarViz")
         .attr("width", x.bandwidth())
         .attr("height", function (d) { return height - y(d.Count); })
         .attr("fill", "#69b3a2")
-
