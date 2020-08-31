6e9b64ac3dc5922894d5e694c267b472dc0f7116
danie <danie@DESKTOP-FML2BVT>
2020-08-31 13:18:31 +0300

    changing the table in user search

--- project/WebAppProject/WebApplicationProject_sucks/Scripts/AdminSearchbar.js
+++ project/WebAppProject/WebApplicationProject_sucks/Scripts/AdminSearchbar.js
@@ -30,7 +30,8 @@ const load = async () => {
     var Birthday = document.getElementsByClassName('birthday');
     var Email = document.getElementsByClassName('email');
     var IsProfessinal = document.getElementsByClassName('isP');
-    var PrifileImage = document.getElementsByClassName('image');
+    var Profession = document.getElementsByClassName('profession');
+    var Score = document.getElementsByClassName('score');
     //
    
 
@@ -43,23 +44,42 @@ const load = async () => {
         var birthday = Birthday.item(i).attributes.getNamedItem("value").nodeValue;
         var email = Email.item(i).attributes.getNamedItem("value").nodeValue;
         var isP = IsProfessinal.item(i).attributes.getNamedItem("value").nodeValue;
-        Users.push({
-            "Name": name,
-            "UserName": userName,
-            "Birthday": birthday,
-            "Email": email,
-            "image": '/src/QutionRoomOwl.png',
-            "IsPro": isP
-        })
+        if (isP == "False") {
+            Users.push({
+                "Name": name,
+                "UserName": userName,
+                "Birthday": birthday,
+                "Email": email,
+                "IsPro": isP,
+                "Profession":"—",
+                "score": "—"
+            })
+        }
+        else {
+            var profession = Profession.item(i).attributes.getNamedItem("value").nodeValue;
+            var score = Score.item(i).attributes.getNamedItem("value").nodeValue;
+            Users.push({
+                "Name": name,
+                "UserName": userName,
+                "Birthday": birthday,
+                "Email": email,
+                "IsPro": isP,
+                "Profession": profession,
+                "score": score
+            })
+        }
+        
     }
     displayUsers(Users);
 }
 const displayUsers = (users) => {
     const htmlString = `<tr>
-        <th>profile image</th>
         <th>User Name</th>
+        <th>Name</th>
         <th>Birthday</th>
-        <th>email</th>
+        <th>Email</th>
+        <th>Profession</th>
+        <th>Score</th>
     </tr>`
     +
     users
@@ -67,10 +87,12 @@ const displayUsers = (users) => {
                 return `
                             <div class="user">
                                <tr><a href="../Users/Details/${user.UserName}">
-                                  <td><img src="${user.image}"></img></td>
                                    <td><h2>${user.UserName}</h2></td>
+                                    <td><h2>${user.Name}</h2></td>
                                     <td><h2>${user.Birthday}</h2></td>
                                      <td><h2>${user.Email}</h2></td>
+                                    <td><h2>${user.Profession}</h2></td>
+                                    <td><h2>${user.score}</h2></td>
                                 </a>
                                 </tr>
                             </div>
